f2ec5818cc3313e48ff86d89a974ca1d2a1e4c4c
smf <silviamourelof@gmail.com>
2018-04-17 21:37:01 +0200

    create second button

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 var button = document.getElementById("button");
+var buttonTwo = document.getElementById("buttonTwo");
 
 
 button.addEventListener("click", moveListOne);
+buttonTwo.addEventListener("click", moveListTwo);
 
 
 function moveListOne() {
     var li = document.querySelector("#text-one li");
     var listTwo = document.querySelector("#text-two");
+
+    move(li, listTwo);
+}
+
+
+function moveListTwo() {
+    var li = document.querySelector("#text-two li");
+    var listTwo = document.querySelector("#text-one");
 
     move(li, listTwo);
 }
